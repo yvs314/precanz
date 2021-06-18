2af90d3208269715e64aa4feb4321195623b1763
Yaroslav Salii <yvs314@gmail.com>
2021-06-18 16:33:21 -0400

    ghc iodir experiment

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-echo "Imma placeholder build.sh"
 
-#ghc -o -O precanz processSOP.hs
-#rm -rf build
-#mkdir build
+rm -rf build
+mkdir build
 #cd build
-#gotta check whether we have to use cmake or cmake3
-#cmake -DCMAKE_BUILD_TYPE=Release ../src
-#make
+ghc -isrc src/processSOP.hs -outputdir build/  -o build/precanz
